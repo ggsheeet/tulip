a7d7fbbb7f3eb9362e9446518fa6a13bdd777b7a
Luis Gzz <ggcryptodoc@gmail.com>
2025-03-14 15:49:11 -0600

    fix: path to rootcert

--- internal/config/config.go
+++ internal/config/config.go
@@ -17,5 +17,5 @@ func GetDatabaseURL() string {
 		return connStr + " sslmode=disable"
 	}
 
-	return connStr + " sslmode=verify-full sslrootcert=/var/lib/postgresql/data/server.crt"
+	return connStr + " sslmode=verify-full sslrootcert=/etc/ssl/certs/server.crt"
 }
